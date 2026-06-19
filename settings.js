e3ae1b38efcd365598aed05e172ff29e3f4936ec
gkompm-hash <gkompm@gmail.com>
2026-06-20 03:19:49 +0500

    adress

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0xCf71bcB9c7B1126FcC70e9C19Fb883505bA5d395";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x5a56912ad5e23977e42caff40473cd4622987cef";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
